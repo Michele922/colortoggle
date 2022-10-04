21b65719f011e3c5397beaf6ded611a00cc14929
Michele <michelesijbring@hotmail.com>
2022-10-04 23:50:26 +0200

    html, css styling nav- sidebar

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// let toggleNavStatus = false;
+
+// // let setBackgroundColor =
+
+// let toggleNav = function () {
+//   let getSidebar = document.querySelector(".nav-sidebar");
+//   let getSidebarUL = document.querySelector(".nav-sidebar ul");
+//   let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+//   if (toggleNavStatus === false) {
+//     getSidebarUL.style.visibility = "visible";
+//     getSidebar.style.width = "272px";
+
+//     let arrayLength = getSidebarLinks.length;
+//     for (let i = 0; i < arrayLength; i++) {
+//       getSidebarLinks[i].style.backgroundcolor = Red;
+//     }
+//   }
+// };
