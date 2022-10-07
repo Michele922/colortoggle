ad85de95a63966d79282a3b7ea2d297fd73bdfc3
Michele <michelesijbring@hotmail.com>
2022-10-07 16:22:04 +0200

    open menu based on classlist

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,23 @@
-// let toggleNavStatus = false;
-
-// // let setBackgroundColor =
-
-// let toggleNav = function () {
-//   let getSidebar = document.querySelector(".nav-sidebar");
-//   let getSidebarUL = document.querySelector(".nav-sidebar ul");
-//   let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
-
-//   if (toggleNavStatus === false) {
-//     getSidebarUL.style.visibility = "visible";
-//     getSidebar.style.width = "272px";
-
-//     let arrayLength = getSidebarLinks.length;
-//     for (let i = 0; i < arrayLength; i++) {
-//       getSidebarLinks[i].style.backgroundcolor = Red;
-//     }
-//   }
-// };
-
 const menu = document.querySelector(".nav-bar"),
   button = document.querySelector(".button"),
-  dropdown = document.querySelector(".sidebar"),
+  sideBar = document.querySelector(".sidebar"),
   sideBarLinks = document.querySelector(".btn-sidebar"),
-  red = document.querySelector(".btn-red"),
-  orange = document.querySelector(".btn-orange"),
-  yellow = document.querySelector(".btn-yellow"),
-  green = document.querySelector(".btn-green"),
-  blue = document.querySelector(".btn-blue"),
-  violet = document.querySelector(".btn-violet");
+  red = document.querySelector(".background-red"),
+  orange = document.querySelector(".background-orange"),
+  yellow = document.querySelector(".background-yellow"),
+  green = document.querySelector(".background-green"),
+  blue = document.querySelector(".background-blue"),
+  violet = document.querySelector(".background-violet");
 
+//Show the menu bar
 button.addEventListener("mouseover", () => {
-  dropdown.style.display = "block";
+  sideBar.classList.add("sidebar-is-open");
 });
 
 //hide the menu bar
 document.addEventListener("click", (event) => {
-  // console.log("is button clicked:", event.target == button);
-
   if (event.target !== button) {
-    dropdown.style.display = "none";
+    sideBar.classList.remove("sidebar-is-open");
   }
 });
 
